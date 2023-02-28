5a12c49281468a317e7f61712fc3673e3e3b49fc
Deorren Erive <erive92d@gmail.com>
2023-02-27 18:07:52 -0800

    added port

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      //heroku - use DB_HOST & DB_PORT
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT
+    }
+  );
+}
 
 module.exports = sequelize;
